fae56e28712df344cd7174ff5a476f4db8d2a1ec
shevdi <shevdi22@rambler.ru>
2025-11-30 18:49:40 +0300

    feat: update theme persistence logic to store current theme in local storage on change

--- client/src/features/ThemeSwitch/ThemeSwitch.tsx
+++ client/src/features/ThemeSwitch/ThemeSwitch.tsx
@@ -4,15 +4,18 @@ import { selectTheme } from './store/selectors'
 import { useSelector } from 'react-redux'
 import { useAppDispatch } from '@/store/hooks'
 import { changeTheme } from './store/themeSlice'
+import { useEffect } from 'react'
 
 const ThemeSwitchContainer = styled.div``
 
 export function ThemeSwitch() {
   const theme = useSelector(selectTheme)
+  useEffect(() => {
+    localStorage.setItem('config', JSON.stringify({ theme }))
+  }, [theme])
   const dispatch = useAppDispatch()
   const onChange = () => {
     dispatch(changeTheme())
-    localStorage.setItem('config', JSON.stringify({ theme: 'dark' }))
   }
   return (
     <ThemeSwitchContainer>
